f9327a41b7e87ea53581b1fcfecbf057389da59c
cactus2004 <107892919+cactus2004@users.noreply.github.com>
2023-11-26 16:19:08 -0700

    Added exception handling

--- WordBank.java
+++ WordBank.java
@@ -1,24 +1,33 @@
-import java.io.FileNotFoundException;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class WordBank {
 
   // This first method implementation is completed for you already.
   // Do not modify the method signature.
-  public static String getAnswerForPuzzleNumber(int puzzleNumber) throws FileNotFoundException {
+  public static String getAnswerForPuzzleNumber(int puzzleNumber){
     // Create a file scanner to read answers.txt.
-    Scanner scanner = new Scanner(new File("answers.txt"));
+    try{
+      Scanner scanner = new Scanner(new File("answers.txt"));
     // Skip the first puzzleNumber number of words in the file.
-    for (int i = 0; i < puzzleNumber; i++) {
-      scanner.next();
-    }
+      for (int i = 0; i < puzzleNumber; i++) {
+        scanner.next();
+      }
+    
     // Return the very next word.
     return scanner.next();
+    }
+
+    catch(FileNotFoundException exception){
+      System.out.println("Answer file not found");
+      return null;
+    }
+
   }
 
   // Do not modify the method signature.
-  public static boolean checkInDictionary(String proposed) throws FileNotFoundException {
+  public static boolean checkInDictionary(String proposed) {
 
     if(proposed.length() != 5){
       return false;
@@ -31,13 +40,20 @@ public class WordBank {
       }
     }
 
-    Scanner scanner = new Scanner(new File("dictionary.txt"));
+    try {
+        Scanner scanner = new Scanner(new File("dictionary.txt"));
 
-    while(scanner.hasNext()){
-      if(scanner.next().equals(proposed)){
-        return true;
-      }
+        while(scanner.hasNext()){
+          if(scanner.next().equals(proposed)){
+          return true;
+        }
+
     }
+      
+    } catch (FileNotFoundException exception) {
+        System.out.println("Dictionary file not found");
+    }
+   
     
     return false;
   
